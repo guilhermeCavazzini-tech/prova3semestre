820f1f955aa3ae5f3a588fcca5f0a099dff16467
Guilherme Soares Cavazzini <107225312+guilhermeCavazzini-tech@users.noreply.github.com>
2023-04-19 23:35:17 -0400

    inicio

---
+++ funcoes.py
@@ -0,0 +1,10 @@
+
+def infosalario ():
+    with open ("liquidemes.txt","w") as arquivos:  
+        arquivos.write('salario R$')
+        arquivos.write(str(salario) +'\n')
+
+infosalario()
+
+
+print("MENU \n 1)INFORMAR RENDIMENTO \n 2)ALTERAR RENDIMENTO \n 3)EXCLUIR RENDIMENTO \n 4)LISTAR RENDIMENTO \n 5)INFORMAR DESPESA \n 6)ALTERAR DESPESA \n 7)REMOVER DESPESA \n")
